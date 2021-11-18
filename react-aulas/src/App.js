aecb81d5db46416f5a65fd7db0bc273d84154cfa
Marcos2k02 <marquinhosmaciel44@gmail.com>
2021-11-17 23:37:42 -0300

    react

---
+++ react-aulas/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+    <h1>Ola-mundo</h1>
+    <h1>Aprendendo react</h1>
+    <img src="https://img.freepik.com/fotos-gratis/imagem-aproximada-em-tons-de-cinza-de-uma-aguia-careca-americana-em-um-fundo-escuro_181624-31795.jpg?size=626&ext=jpg" alt=""></img>
+    </div>
+  );
+}
+
+export default App;
